2d4702889e6774808c28aae0b3b7fcabbf5393e1
bilel <bilel.brahim@gmail.com>
2019-10-07 21:30:50 +0200

    refactoring

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -3,7 +3,7 @@
 # Usage (from root of project): sh ./Docker/docker-build-push.sh
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t bilel/storefront-accounts:gke-2.2.0 .
-docker push bilel/storefront-accounts:gke-2.2.0
+docker build -f Docker/Dockerfile --no-cache -t bilel/accounts:gke-2.2.0 .
+docker push bilel/accounts:gke-2.2.0
 
-# docker run --name storefront-accounts -d bilel/storefront-accounts:gke-2.2.0
+# docker run --name accounts -d bilel/accounts:gke-2.2.0
